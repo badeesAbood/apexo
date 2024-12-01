5d518337da398ebeceaae70ff77294f79814630c
elselawi <ali.a.saleem@outlook.com>
2024-12-01 13:25:24 +0300

    colors for logging

--- lib/backend/utils/logger.dart
+++ lib/backend/utils/logger.dart
@@ -3,20 +3,20 @@ import 'package:logging/logging.dart';
 final log = Logger('ApexoLogger');
 
 /// 1 = severe, 2 = warning, 3 = info
-void logger(Object msg, StackTrace stacktrace, [int importance = 0]) {
+void logger(Object msg, StackTrace? stacktrace, [int importance = 0]) {
   switch (importance) {
     case 1:
-      log.severe(msg);
+      log.severe("\x1B[31m$msg\x1B[0m");
       break;
     case 2:
-      log.warning(msg);
+      log.warning("\x1B[33m$msg\x1B[0m");
       break;
     case 3:
-      log.info(msg);
+      log.info("\x1B[34m$msg\x1B[0m");
       break;
     default:
-      log.severe(msg);
+      log.severe("\x1B[31m$msg\x1B[0m");
       break;
   }
-  log.info(stacktrace);
+  if (stacktrace != null) log.info("\n\x1B[35mSTACKTRACE:\n$stacktrace\x1B[0m");
 }
