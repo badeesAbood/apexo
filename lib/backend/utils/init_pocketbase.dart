c3ffce29c25c80d9aaa8d2c1efc28d69690a1291
elselawi <ali.a.saleem@outlook.com>
2024-12-13 23:48:30 +0300

    updated createRule for users to be explicitly for admins only

--- lib/backend/utils/init_pocketbase.dart
+++ lib/backend/utils/init_pocketbase.dart
@@ -4,7 +4,7 @@ import 'package:pocketbase/pocketbase.dart';
 /// This would initialize the server creating required collections, rules, and settings
 initializePocketbase(PocketBase pb) async {
   await pb.collections.import([dataCollectionImport, publicCollectionImport]);
-  await pb.collections.update("users", body: {"createRule": null});
+  await pb.collections.update("users", body: {"createRule": '@request.auth.collectionName = "_superusers"'});
   await pb.settings.update(body: {
     "batch": {"enabled": true, "maxRequests": 101, "timeout": 3, "maxBodySize": 0}
   });
