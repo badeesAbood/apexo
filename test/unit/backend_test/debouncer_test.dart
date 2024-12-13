bc74dc6645afd8df688fa685e5dd820f8d5599a2
elselawi <ali.a.saleem@outlook.com>
2024-12-13 23:17:29 +0300

    debouncer -> Debouncer

--- test/unit/backend_test/debouncer_test.dart
+++ test/unit/backend_test/debouncer_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:apexo/backend/utils/Debouncer.dart';
+import 'package:apexo/backend/utils/debouncer.dart';
 
 void main() {
   group('Debouncer', () {
